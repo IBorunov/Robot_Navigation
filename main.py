d38932af2fc8f1ed16f7df6a727c53a2810bf99d
IBorunov <ivanborun@gmail.com>
2024-06-27 11:59:02 +0300

    Function to define neighbouring nodes added

--- main.py
+++ main.py
@@ -22,3 +22,13 @@ def generate_map(size, obstacle_chance=0.3):
     return _map
 
 
+def find_neighbouring_nodes(node, _map):
+    """Функция определения соседних узлов"""
+    neighbours = []
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # вверх, вниз, влево, вправо
+    for direction in directions:
+        neighbour = (node[0] + direction[0], node[1] + direction[1])
+        # проверяем, не выходим ли мы за границы карты
+        if 0 < neighbour[0] < len(_map) and 0 < neighbour[1] < len(_map):
+            neighbours.append(neighbour)
+    return neighbours
