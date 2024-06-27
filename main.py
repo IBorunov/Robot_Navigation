c73239be775fbfcfd8ede50c1a5d5b826b68e786
IBorunov <ivanborun@gmail.com>
2024-06-27 12:29:21 +0300

    Main A* algorithm made

--- main.py
+++ main.py
@@ -32,3 +32,41 @@ def find_neighbouring_nodes(node, _map):
         if 0 < neighbour[0] < len(_map) and 0 < neighbour[1] < len(_map):
             neighbours.append(neighbour)
     return neighbours
+
+
+def heuristic(node, goal):
+    """ Эвристическая функция для A* (манхэттенское расстояние) """
+    return abs(node[0] - goal[0]) + abs(node[1] - goal[1])
+
+
+def a_star(_map, start, goal):
+    """
+    Реализация алгоритма A* для поиска кратчайшего пути на сетке.
+    Возвращает:
+    list of tuple of int - содержащий кратчайший путь от start до goal, или пустой список, если путь не найден.
+    """
+    open_set = []
+    heapq.heappush(open_set, (0, start))  # Начальная точка добавляется в open_set с оценкой 0
+    came_from = {}  # Словарь для сохранения пути
+    g_score = {start: 0}  # Стоимость пути от начала до текущей точки
+    f_score = {start: heuristic(start, goal)}  # Оценочная стоимость от начала до цели через текущую точку
+
+    while open_set:
+        current = heapq.heappop(open_set)[1]
+
+        if current == goal:
+            return reconstruct_path(came_from, current)  # Если текущий узел - цель, восстанавливаем путь
+
+        for neighbour in find_neighbouring_nodes(current, _map):
+            if _map[neighbour[0]][neighbour[1]] == 1:
+                continue  # Пропускаем, если соседний узел - препятствие
+
+            tentative_g_score = g_score[current] + 1  # Предварительная стоимость пути до соседнего узла
+
+            if neighbour not in g_score or tentative_g_score < g_score[neighbour]:
+                came_from[neighbour] = current
+                g_score[neighbour] = tentative_g_score
+                f_score[neighbour] = tentative_g_score + heuristic(neighbour, goal)
+                heapq.heappush(open_set, (f_score[neighbour], neighbour))  # Добавляем соседний узел в open_set
+
+    return []  # Если все узлы обработаны и цель не достигнута, возвращаем пустой список
